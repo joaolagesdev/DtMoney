164a3bbc921373d60c122f7861d51f8f30eb9e66
João Artur <67400301+joaothomaz@users.noreply.github.com>
2021-03-17 08:59:10 -0300

    config fake api with miragejs

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,32 +1,40 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 
 export function TransactionTable() {
-  return (
-		<Container>
-			<table>
-				<thead>
-					<tr>
-						<th>Título</th>
-						<th>Valor</th>
-						<th>Categoria</th>
-						<th>Data</th>
-					</tr>
-				</thead>
+	useEffect(() => {
+    fetch("http://localhost:3000/api/transactions")
+			.then(response => response.json())
+			.then(data => console.log(data))
+  }, []);
 
-				<tbody>
-					<tr>
-						<td>Desenvolvimento de website</td>
-						<td className="deposit">R$12.000</td>
-						<td>Desenvolvimento</td>
-						<td>20/02/2021</td>
-					</tr>
-					<tr>
-						<td>Aluguel</td>
-						<td className="withdraw">-R$1.000</td>
-						<td>Casa</td>
-						<td>17/02/2021</td>
-					</tr>
-				</tbody>
-			</table>
-		</Container>)
+  return (
+  	<Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">-R$1.000</td>
+            <td>Casa</td>
+            <td>17/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
 }
